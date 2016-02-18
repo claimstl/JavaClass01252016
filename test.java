83d32ad866aa57d9615236c4411cff7969a17443
Santosh Tokekar <stokekar@gmail.com>
2016-02-17 22:17:18 -0600

    modified bug123

--- test.java
+++ test.java
@@ -1,8 +1,6 @@
 public class test{
 	public static void main(String[] args){
 		//bug123 fix code here 
-		//without changing branch to master
-		//(git checkout master)
 		System.out.println("hello world");
 	}
 
