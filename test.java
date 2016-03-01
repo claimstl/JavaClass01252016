a3c15bf1dee38bd3b0682e0c2af06ff3a5d9e245
santoshst <stokekar@gmail.com>
2016-02-29 18:47:18 -0600

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test{
 	public static void main(String[] args){
-//following changes are for April release
+//following changes are for April release test
 		System.out.println("hello world");
 	}
 
